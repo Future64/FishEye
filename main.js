219e297799918c507e829a215d03275be2ea1fd3
Maxim <developement.future@gmail.com>
2021-05-27 00:25:19 +0200

    création de la class Photographe

---
+++ main.js
@@ -0,0 +1,10 @@
+class Photographe {
+
+    constructor(name, location, citation, price, tag) {
+        this.name = name;
+        this.location = location;
+        this.citation = citation;
+        this.price = price;
+        this.tag = tag;
+    }
+}
