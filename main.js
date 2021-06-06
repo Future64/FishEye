7a8ef874cbfc7956a71db4339f4beac02bb6f43d
Maxim <developement.future@gmail.com>
2021-06-07 00:05:54 +0200

    création card en js

--- main.js
+++ main.js
@@ -35,26 +35,56 @@ fetch('/data/photographers.json')
         console.log([obj.name])
     })
 
-// Créer une div
-const createDiv = (className, monId) => {
-    const div = document.createElement('div')
-    div.classList.add(className)
-    div.id = monId
-    return div
-}
+// Créer un DOM Element
+const createDomElement = (className, DomElem) => {
+        const elm = document.createElement(DomElem)
+        div.classList.add(className)
+        return elm
+    }
+    // for (let i = 0; i < Array.length; i++) {}
 
-// Créer un component
-const createComponent = (type, className, url) => {
-    const type = document.createElement(type)
-    type.classList.add(className)
-    type.src = url
-    return type
-}
+const section = document.querySelector(".index--section")
 
-const indexSection = document.querySelector(".index--section")
-const indexCard = createDiv(indexSection)
-indexSection.appendChild(indexCard)
 
-const p = createComponent(msg)
-const msg = "C'est un truc de ouf !"
-indexCard.appendChild(p)
+const nameL = "Mimi Keel"
+const locaTion = "London, UK"
+const citation = "Voir le beau dans le quotidien"
+const price = "400€/jour"
+
+const containerCard = document.createElement('div')
+containerCard.className = "containerCard"
+const indexCard = document.createElement('div')
+indexCard.className = "index--card"
+    // indexCard.id = 'photographe' + i
+const headerCard = document.createElement('div')
+headerCard.className = "index--header-card"
+const photoProfil = document.createElement('img')
+photoProfil.className = "photoProfil"
+photoProfil.src = "./medias/Mimi/Portrait_Nora.jpg"
+const nameHeader = document.createElement('p')
+nameHeader.className = "name--header-card"
+
+const indexBodyCard = document.createElement('div')
+indexBodyCard.className = "index--body-card"
+const locationBodyCard = document.createElement('p')
+locationBodyCard.className = "location--body-card"
+const citationBodyCard = document.createElement('p')
+citationBodyCard.className = "citation--body-card"
+const priceBodyCard = document.createElement('p')
+priceBodyCard.className = "price--body-card"
+
+nameHeader.innerHTML = nameL
+locationBodyCard.innerHTML = locaTion
+citationBodyCard.innerHTML = citation
+priceBodyCard.innerHTML = price
+
+section.append(containerCard)
+
+containerCard.append(indexCard)
+containerCard.append(indexBodyCard)
+indexCard.append(headerCard)
+headerCard.append(photoProfil)
+headerCard.append(nameHeader)
+indexBodyCard.append(locationBodyCard)
+indexBodyCard.append(citationBodyCard)
+indexBodyCard.append(priceBodyCard)
