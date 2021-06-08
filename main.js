01896aa4916d9eff9367b3faf5cee4e3dfee1a9a
Maxim <developement.future@gmail.com>
2021-06-08 18:24:34 +0200

    comentaires et mise en page du code

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
-// Récupère le Json
+// Méthode de Récuperation de l'objet avec le chemin
 fetch('/data/photographers.json')
+    // demander de retouner l'objet au format Json
     .then((response) => {
         return response.json()
     })
+    // Fonction de récupération et d'utilisation des informations du Json
     .then((obj) => {
         for (let i = 0; i < obj.photographers.length; i++) {
 
+            // selection de la balise Hml
             const section = document.querySelector(".index--section")
 
+            // Creation des DomElements
             const containerCard = document.createElement('div')
             const indexCard = document.createElement('div')
-            containerCard.id = 'photographe' + i
-            console.log(containerCard.id)
             const headerCard = document.createElement('div')
             const photoProfil = document.createElement('img')
             const nameHeader = document.createElement('p')
@@ -20,6 +22,22 @@ fetch('/data/photographers.json')
             const citationBodyCard = document.createElement('p')
             const priceBodyCard = document.createElement('p')
 
+            // Création des ID et classes 
+            containerCard.id = 'photographe' + i
+            containerCard.className = "containerCard"
+            indexCard.className = "index--card"
+            headerCard.className = "index--header-card"
+            photoProfil.className = "photoProfil" + i
+            nameHeader.className = "name--header-card"
+            indexBodyCard.className = "index--body-card"
+            locationBodyCard.className = "location--body-card"
+            citationBodyCard.className = "citation--body-card"
+            priceBodyCard.className = "price--body-card"
+
+            //Initialisation de la variable url
+            let url = ""
+
+            //Initialisation 
             const photoProfilArray = [
                 "./medias/photoProfil/Portrait_Nora.jpg",
                 "./medias/photoProfil/Architecture_Horseshoe.jpg",
@@ -29,8 +47,8 @@ fetch('/data/photographers.json')
                 "./medias/photoProfil/Travel_Tower.jpg"
             ]
 
-            let url = ""
 
+            // Boucle pour assigner la photo de profil à chaque cards
             for (let j = 0; j < photoProfilArray.length; j++) {
 
                 if (obj.photographers[i] == obj.photographers[0]) {
@@ -48,24 +66,17 @@ fetch('/data/photographers.json')
                 }
             }
 
+            // Assigner la variable url pour les chemins des la photos de profils
             photoProfil.src = url
 
-            containerCard.className = "containerCard"
-            indexCard.className = "index--card"
-            headerCard.className = "index--header-card"
-            photoProfil.className = "photoProfil" + i
-            nameHeader.className = "name--header-card"
-            indexBodyCard.className = "index--body-card"
-            locationBodyCard.className = "location--body-card"
-            citationBodyCard.className = "citation--body-card"
-            priceBodyCard.className = "price--body-card"
-
+            // Afficher les informations dans les DomElements
             photoProfil.innerHTML = url
             nameHeader.innerHTML = obj.photographers[i].name
             locationBodyCard.innerHTML = obj.photographers[i].city + ", " + obj.photographers[i].country
             citationBodyCard.innerHTML = obj.photographers[i].tagline
             priceBodyCard.innerHTML = obj.photographers[i].price + "€"
 
+            // Attacher les DomElements entre eux
             section.append(containerCard)
             containerCard.append(indexCard)
             containerCard.append(indexBodyCard)
@@ -75,9 +86,10 @@ fetch('/data/photographers.json')
             indexBodyCard.append(locationBodyCard)
             indexBodyCard.append(citationBodyCard)
             indexBodyCard.append(priceBodyCard)
-
         }
     })
+
+/**°°°°°°°°°°°°   FUNCTION   °°°°°°°°°°°°°°°°°°°°°°°° */
 
 // Créer un DOM Element
 const createDomElement = (className, DomElem) => {
