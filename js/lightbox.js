40a91c4f20dded42662b3652d543c7a86b2944d8
Maxim <developement.future@gmail.com>
2021-09-27 17:19:35 +0200

    ajout prefixer npm et title dans lightbox etc

--- js/lightbox.js
+++ js/lightbox.js
@@ -2,19 +2,20 @@ import { enableBodyScroll, disableBodyScroll } from './body-scroll-lock.js'
 
 /**
  * @property {HTMLElement} element
- * @property {string[]} medias Chemins des medias de la lightbox
+ * @property {string[]} uri Chemins des uri de la lightbox
  * @property {string} url Media actuellement affichée
  **/
 export class Lightbox {
 
     /**
      * @param {string} url URL du media
-     * @param {string[]} medias Chemins des medias de la lightbox
+     * @param {string[]} uri Chemins des uri de la lightbox
      */
-    constructor(url, medias) {
+    constructor(url, uri) {
+        this.data = JSON.parse(localStorage.getItem('data'))
         this.lightboxMain = document.body.querySelector(".lightbox-main")
         this.element = this.buildDOM(url)
-        this.medias = medias
+        this.uri = uri
         this.loadImage(url)
         this.onKeyUp = this.onKeyUp.bind(this)
         this.lightboxMain.appendChild(this.element)
@@ -23,8 +24,6 @@ export class Lightbox {
     }
 
     static init() {
-        let data = localStorage.getItem('data')
-        data = JSON.parse(data)
         const links = Array.from(document.querySelectorAll('img[src$=".jpg"], video[src$=".mp4"]'))
         const gallery = links.map(link => link.getAttribute('src'))
         links.forEach(link => link.addEventListener('click', e => {
@@ -48,8 +47,19 @@ export class Lightbox {
     loadImage(url) {
         this.url = url
         const container = this.element.querySelector('.lightboxImgContainer')
+        const lightboxTitle = this.element.querySelector('.lightboxTitle')
         const img = document.createElement('img')
         const video = document.createElement('video')
+
+        const splitUrl = this.url.split('/')
+        const mediaName = splitUrl[splitUrl.length - 1];
+        let metaMedia = null
+
+        this.data.media.forEach(media => {
+            if (media.image == mediaName) {
+                metaMedia = media
+            }
+        });
 
         container.innerHTML = ''
 
@@ -58,6 +68,12 @@ export class Lightbox {
             img.setAttribute("src", url)
             container.appendChild(img)
             img.onload = () => { this.url = url }
+
+            this.data.media.forEach(media => {
+                if (media.image == mediaName) {
+                    metaMedia = media
+                }
+            });
         } else {
             video.classList.add('lightboxVideo')
             video.setAttribute("src", url)
@@ -65,7 +81,14 @@ export class Lightbox {
             video.controls = true
             container.appendChild(video)
             video.onload = () => { this.url = url }
+            this.data.media.forEach(media => {
+                if (media.video == mediaName) {
+                    metaMedia = media
+                }
+            });
         }
+
+        lightboxTitle.innerHTML = metaMedia.title
 
         this.lightboxMain.style.display = "block"
 
@@ -106,11 +129,11 @@ export class Lightbox {
      */
     next(e) {
         e.preventDefault()
-        let i = this.medias.findIndex(media => media === this.url)
-        if (i === this.medias.length - 1) {
+        let i = this.uri.findIndex(media => media === this.url)
+        if (i === this.uri.length - 1) {
             i = -1
         }
-        this.loadImage(this.medias[i + 1])
+        this.loadImage(this.uri[i + 1])
     }
 
     /**
@@ -118,11 +141,11 @@ export class Lightbox {
      */
     prev(e) {
         e.preventDefault()
-        let i = this.medias.findIndex(media => media === this.url)
+        let i = this.uri.findIndex(media => media === this.url)
         if (i === 0) {
-            i = this.medias.length
+            i = this.uri.length
         }
-        this.loadImage(this.medias[i - 1])
+        this.loadImage(this.uri[i - 1])
     }
 
     /**
@@ -135,13 +158,15 @@ export class Lightbox {
         dom.innerHTML = `<div class="lightboxContainer" aria-label="image closeup view">
                             <button type="button" class="btnLightboxFAS">
                                 <i class="fas fa-chevron-left btnLightbox" alt="Previous image"></i>
-                            <button/>
+                            </button>
                             <div class="lightboxImgContainer"></div>
                             <button type="button" class="btnLightboxFAS">
                                 <i class="fas fa-chevron-right btnLightbox" alt="Next image"></i>
-                            <button/>
+                            </button>
                             <i class="fas fa-times lightboxClose" alt="Close dialog"></i>
-                            <div class="lightboxTitle"></div>
+                            <div class="lightboxTitleContainer">
+                                <div class="lightboxTitle"></div>
+                            </div>
                         </div>`
         dom.querySelector('.lightboxClose').addEventListener('click', this.close.bind(this))
         dom.querySelector('.fas.fa-chevron-right').addEventListener('click', this.next.bind(this))
