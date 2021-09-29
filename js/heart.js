b154abc82e8031d8289c5baf903bca7fd0ec5891
Maxim <developement.future@gmail.com>
2021-09-29 02:04:59 +0200

    Css sur logo photographe et accesssibilité sur heart

--- js/heart.js
+++ js/heart.js
@@ -38,5 +38,32 @@ export const incrementTotalNbLikes = (total) => {
             // on affiche le total
             displayHeart(total)
         })
+
+        heart.addEventListener('keydown', (event) => {
+            if (event.key === 'Enter') {
+                // on récupère le parent comme on peut (grace à l'événement ici)
+                const parent = event.path[1];
+                let picLike = parent.querySelector('.mediaNbLike');
+
+                // si le coeur est vide
+                if (heart.classList.contains('far')) {
+                    // on incrémente le nb de like
+                    picLike.innerHTML = parseInt(picLike.innerHTML) + 1;
+                    // on incrémente le nb de like total
+                    total += 1;
+                    // on change le coeur
+                    heart.classList.replace("far", "fas");
+                } else {
+                    // on décrémente le nb de like
+                    picLike.innerHTML = parseInt(picLike.innerHTML) - 1;
+                    // on décrémente le nb de like total
+                    total -= 1;
+                    // on change le coeur
+                    heart.classList.replace("fas", "far");
+                }
+                // on affiche le total
+                displayHeart(total)
+            }
+        })
     });
 }
